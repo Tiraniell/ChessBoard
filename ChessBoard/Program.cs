dc13a46ef38ce055b5aa9adfc513be58a65d3d19
Tiraniell <93056140+Tiraniell@users.noreply.github.com>
2023-01-28 17:55:41 +0300

    Добавьте файлы проекта.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = '#';
+            int chessCage = 8;
+
+            for (int i = 0; i <= chessCage; i++)
+            {
+               
+                for (int j = 0; j <= chessCage; j++)
+                {
+                    Console.Write($"{ch}");
+
+                    //if(i == ch && j == ch)
+                    //{
+                    //    ch = ' ';
+                    //    Console.Write($"{ch}");
+                    //}
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.SetCursorPosition(10, 10);
+        }
+    }
+}
